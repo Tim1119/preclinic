1295770cc6e0b23c3a4f4e13c83df00d34f81af8
Ajani Timothy <timothyinioluwa@gmail.com>
2024-07-12 09:08:21 +0100

    fix: configured django logger

--- preclinic/settings/base.py
+++ preclinic/settings/base.py
@@ -154,37 +154,38 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+if ('DEBUG') == 'True':
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+                },
+                "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+                "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
             },
-            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "console",
+                },
+                "file": {
+                    "level": "INFO",
+                    "class": "logging.FileHandler",
+                    "formatter": "file",
+                    "filename": "logs/preclinic.log",
+                },
+                "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
             },
-            "file": {
-                "level": "INFO",
-                "class": "logging.FileHandler",
-                "formatter": "file",
-                "filename": BASE_DIR / "logs/preclinic.log",
+            "loggers": {
+                "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+                "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+                "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
             },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
-            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-        },
-    }
-)
+        }
+    )
 
 
 
