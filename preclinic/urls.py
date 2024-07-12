ddfef4327b279c24e460ee4987a9207b3b8ece66
Ajani Timothy <timothyinioluwa@gmail.com>
2024-07-12 13:20:47 +0100

    fix:added login_url

--- preclinic/urls.py
+++ preclinic/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.accounts.urls',namespace='accounts')),
+    path('', include('apps.accounts.urls',namespace='accounts')),
     path('profiles/', include('apps.profiles.urls',namespace='profiles')),
     path('utilities/', include('apps.utilities.urls',namespace='utilities')),
     path('appointments/', include('apps.appointments.urls',namespace='appointments')),
