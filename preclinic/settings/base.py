8cd29546a67a3e87b88a60545296771843d443d5
Ajani Timothy <timothyinioluwa@gmail.com>
2024-07-12 10:07:53 +0100

    fix: added white noise

--- preclinic/settings/base.py
+++ preclinic/settings/base.py
@@ -62,7 +62,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 ROOT_URLCONF = 'preclinic.urls'
 
